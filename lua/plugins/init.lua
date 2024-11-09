7f8931c026447485ff2d62295bceff0e22169e16
Aperiax <arianadamott@gmail.com>
2024-11-09 17:50:11 +0100

    just fixing typos

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -83,7 +83,7 @@ return {
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-      vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+      vim.keymap.set("n", "<leader>gl", builtin.git_branches, {})
       require("telescope").load_extension "ui-select"
     end,
   },
